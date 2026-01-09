01638eba7bb8c06a2c867521c845213c788d5091
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2026-01-09 08:09:31 +0200

    Added common regex for validation purposes

--- pkg/validator/validator.go
+++ pkg/validator/validator.go
@@ -1,5 +1,16 @@
 package validator
 
+import "regexp"
+
+var (
+	EmailRX    = regexp.MustCompile(`^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$`)
+	LowerRX    = regexp.MustCompile(`[a-z]`)
+	AlphanumRX = regexp.MustCompile(`^[a-zA-Z0-9]+$`)
+	UpperRX    = regexp.MustCompile(`[A-Z]`)
+	NumberRX   = regexp.MustCompile(`[0-9]`)
+	SpecialRX  = regexp.MustCompile(`[!@#$%^&*]`)
+)
+
 // Validator used for producing errors in key-value format. it's mainly used
 // for struct validation.
 type Validator struct {
