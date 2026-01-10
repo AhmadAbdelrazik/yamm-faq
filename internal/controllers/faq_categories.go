4d38f7e072b2c506808fdbd1e62d25675e96066d
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2026-01-11 00:04:26 +0200

    Implemented FAQ Categories Handlers and added them to routes

---
+++ internal/controllers/faq_categories.go
@@ -0,0 +1,184 @@
+package controllers
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/AhmadAbdelrazik/yamm_faq/internal/httputil"
+	"github.com/AhmadAbdelrazik/yamm_faq/internal/models"
+	"github.com/AhmadAbdelrazik/yamm_faq/internal/services"
+	"github.com/AhmadAbdelrazik/yamm_faq/pkg/validator"
+	"github.com/gin-gonic/gin"
+)
+
+func (c *Controller) createFaqCategoryHandler(ctx *gin.Context) {
+	user := ctx.MustGet(userContextKey).(*models.User)
+
+	var input createFaqCategoryInput
+	if err := ctx.BindJSON(&input); err != nil {
+		httputil.BadRequest(ctx, err)
+		return
+	}
+
+	category, err := c.Services.FAQCategories.Create(services.CreateCategoryInput{
+		Admin:        user,
+		CategoryName: input.Name,
+	})
+
+	if err != nil {
+		switch {
+		case errors.Is(err, services.ErrUnauthorized):
+			httputil.NewError(ctx, http.StatusForbidden, err)
+		case errors.Is(err, services.ErrCategoryAlreadyExists):
+			httputil.NewError(ctx, http.StatusConflict, err)
+		case errors.Is(err, validator.ErrInvalid):
+			httputil.InvalidEntity(ctx, err)
+		default:
+			httputil.InternalServerError(ctx, err)
+		}
+	}
+
+	ctx.JSON(
+		http.StatusCreated,
+		createFaqCategoryResponse{
+			Message: fmt.Sprintf("category %v created successfully", category.Name),
+		},
+	)
+}
+
+func (c *Controller) getAllFaqCategoryHandler(ctx *gin.Context) {
+	categories, err := c.Services.FAQCategories.GetAll()
+	if err != nil {
+		httputil.InternalServerError(ctx, err)
+		return
+	}
+
+	fmt.Printf("categories: %v\n", categories)
+	dto := make([]faqCategoryDTO, len(categories))
+
+	for i := range dto {
+		dto[i] = faqCategoryDTO(categories[i])
+	}
+	fmt.Printf("dto: %v\n", dto)
+
+	ctx.JSON(http.StatusOK, getAllFaqCategoryResponse{dto})
+}
+
+func (c *Controller) getFaqCategoryHandler(ctx *gin.Context) {
+	categoryName := ctx.Param("category")
+
+	category, err := c.Services.FAQCategories.Find(categoryName)
+	if err != nil {
+		switch {
+		case errors.Is(err, services.ErrCategoryNotFound):
+			httputil.NotFound(ctx, err)
+		default:
+			httputil.InternalServerError(ctx, err)
+		}
+		return
+	}
+
+	ctx.JSON(http.StatusOK, faqCategoryDTO(*category))
+}
+
+func (c *Controller) updateFaqCategoryHandler(ctx *gin.Context) {
+	user := ctx.MustGet(userContextKey).(*models.User)
+
+	oldCategoryName := ctx.Param("category")
+
+	var input updateFaqCategoryInput
+	if err := ctx.BindJSON(&input); err != nil {
+		httputil.BadRequest(ctx, err)
+		return
+	}
+
+	category, err := c.Services.FAQCategories.Update(services.UpdateCategoryInput{
+		Admin:   user,
+		OldName: oldCategoryName,
+		NewName: input.NewName,
+	})
+
+	if err != nil {
+		switch {
+		case errors.Is(err, services.ErrCategoryNotFound):
+			httputil.NotFound(ctx, err)
+		case errors.Is(err, services.ErrUnauthorized):
+			httputil.NewError(ctx, http.StatusForbidden, err)
+		case errors.Is(err, services.ErrCategoryAlreadyExists),
+			errors.Is(err, services.ErrCategoryEditConflict):
+			httputil.NewError(ctx, http.StatusConflict, err)
+		case errors.Is(err, validator.ErrInvalid):
+			httputil.InvalidEntity(ctx, err)
+		default:
+			httputil.InternalServerError(ctx, err)
+		}
+		return
+	}
+
+	ctx.JSON(
+		http.StatusOK,
+		updateFaqCategoryResponse{
+			Message: fmt.Sprintf("category %v updated successfully to %v", oldCategoryName, category.Name),
+		},
+	)
+}
+
+func (c *Controller) deleteFaqCategoryHandler(ctx *gin.Context) {
+	user := ctx.MustGet(userContextKey).(*models.User)
+
+	category := ctx.Param("category")
+
+	err := c.Services.FAQCategories.Delete(services.DeleteCategoryName{
+		Admin:        user,
+		CategoryName: category,
+	})
+
+	if err != nil {
+		switch {
+		case errors.Is(err, services.ErrUnauthorized):
+			httputil.NewError(ctx, http.StatusForbidden, err)
+		case errors.Is(err, services.ErrCategoryNotFound):
+			httputil.NotFound(ctx, err)
+		default:
+			httputil.InternalServerError(ctx, err)
+		}
+		return
+	}
+
+	ctx.JSON(
+		http.StatusOK,
+		deletedFaqCategoryResponse{
+			Message: fmt.Sprintf("deleted %v category successfully", category),
+		},
+	)
+}
+
+type createFaqCategoryInput struct {
+	Name string `json:"name"`
+}
+
+type createFaqCategoryResponse struct {
+	Message string `json:"message"`
+}
+
+type getAllFaqCategoryResponse struct {
+	Categories []faqCategoryDTO `json:"categories"`
+}
+
+type faqCategoryDTO struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type updateFaqCategoryInput struct {
+	NewName string `json:"new_name"`
+}
+
+type updateFaqCategoryResponse struct {
+	Message string `json:"message"`
+}
+
+type deletedFaqCategoryResponse struct {
+	Message string `json:"message"`
+}
--- internal/controllers/routes.go
+++ internal/controllers/routes.go
@@ -10,10 +10,21 @@ import (
 func (c *Controller) Routes(r *gin.Engine) {
 	api := r.Group("/api/v1")
 
+	auth := api.Group("/")
+	auth.Use(c.authMiddleware())
+
 	// users
 	api.POST("/signup/customer", c.signupCustomerHandler)
 	api.POST("/signup/merchant", c.signupMerchantHandler)
 	api.POST("/login", c.loginHandler)
+
+	// FAQ Categories
+	api.GET("/faq-categories/:category", c.getFaqCategoryHandler)
+	api.GET("/faq-categories", c.getAllFaqCategoryHandler)
+
+	auth.POST("/faq-categories", c.createFaqCategoryHandler)
+	auth.PUT("/faq-categories/:category", c.updateFaqCategoryHandler)
+	auth.DELETE("/faq-categories/:category", c.deleteFaqCategoryHandler)
 
 	api.GET("/health", c.healthCheckHandler)
 }
