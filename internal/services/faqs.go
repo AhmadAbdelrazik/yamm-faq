58d87780f37e4b4cb3a391300ebc1c635017eade
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2026-01-11 23:39:57 +0200

    Implemented Service Layer for FAQs

--- internal/services/faqs.go
+++ internal/services/faqs.go
@@ -1,11 +1,16 @@
 package services
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/AhmadAbdelrazik/yamm_faq/internal/models"
 	"github.com/AhmadAbdelrazik/yamm_faq/internal/repositories"
 	"github.com/AhmadAbdelrazik/yamm_faq/pkg/validator"
+)
+
+var (
+	ErrFaqNotFound = errors.New("faq not found")
 )
 
 type FaqServices struct {
@@ -72,4 +77,166 @@ func (s *FaqServices) CreateGlobalFaq(input CreateGlobalFaqInput) (*models.FAQ, 
 	}
 
 	return faq, nil
+}
+
+func (s *FaqServices) GetGlobalFaqs(category *models.FAQCategory) ([]models.FAQ, error) {
+	return s.repos.Faqs.GetAllByCategory(category.Name)
+}
+
+func (s *FaqServices) GetStoreFaqs(store *models.Store) ([]models.FAQ, error) {
+	return s.repos.Faqs.GetAllByStore(store.ID)
+}
+
+// MerchantUpdateFaq updates FAQ related to merchant's store.
+func (s *FaqServices) MerchantUpdateFaq(input MerchantUpdateFaqInput) (*models.FAQ, error) {
+	if input.Merchant.ID != input.Store.MerchantID {
+		return nil, ErrUnauthorized
+	}
+
+	faq, err := s.repos.Faqs.FindDefault(input.FAQID)
+	if err != nil {
+		switch {
+		case errors.Is(err, repositories.ErrNotFound):
+			return nil, ErrFaqNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	if input.Store.ID != faq.StoreID {
+		return nil, ErrUnauthorized
+	}
+
+	faq.Translations[0].Question = input.Question
+	faq.Translations[0].Answer = input.Answer
+	faq.Translations[0].Language = models.Language(input.Language)
+
+	faq.Category = *input.Category
+	faq.DefaultLanguage = models.Language(input.Language)
+
+	v := validator.New()
+	if faq.Validate(v); !v.Valid() {
+		return nil, v.Err()
+	}
+
+	if err := s.repos.Faqs.Update(faq); err != nil {
+		return nil, err
+	}
+	if err := s.repos.Translations.Update(&faq.Translations[0]); err != nil {
+		return nil, err
+	}
+
+	return faq, nil
+}
+
+// AdminUpdateFaq updates FAQ related to merchant's store.
+func (s *FaqServices) AdminUpdateFaq(input AdminUpdateFaqInput) (*models.FAQ, error) {
+	if !input.Admin.IsAdmin() {
+		return nil, ErrUnauthorized
+	}
+
+	faq, err := s.repos.Faqs.FindDefault(input.FAQID)
+	if err != nil {
+		switch {
+		case errors.Is(err, repositories.ErrNotFound):
+			return nil, ErrFaqNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	faq.Translations[0].Question = input.Question
+	faq.Translations[0].Answer = input.Answer
+	faq.Translations[0].Language = models.Language(input.Language)
+
+	faq.Category = *input.Category
+	faq.DefaultLanguage = models.Language(input.Language)
+	faq.IsGlobal = input.IsGlobal
+
+	v := validator.New()
+	if faq.Validate(v); !v.Valid() {
+		return nil, v.Err()
+	}
+
+	if err := s.repos.Faqs.Update(faq); err != nil {
+		return nil, err
+	}
+	if err := s.repos.Translations.Update(&faq.Translations[0]); err != nil {
+		return nil, err
+	}
+
+	return faq, nil
+}
+
+func (s *FaqServices) AdminDelete(user *models.User, faqID int) error {
+	if !user.IsAdmin() {
+		return ErrUnauthorized
+	}
+
+	err := s.repos.Faqs.Delete(faqID)
+	if err != nil {
+		switch {
+		case errors.Is(err, repositories.ErrNotFound):
+			return ErrFaqNotFound
+		default:
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *FaqServices) MerchantDelete(merchant *models.User, store *models.Store, faqID int) error {
+	if merchant.ID != store.MerchantID {
+		return ErrUnauthorized
+	}
+
+	err := s.repos.Faqs.MerchantDelete(faqID, store.ID)
+	if err != nil {
+		switch {
+		case errors.Is(err, repositories.ErrNotFound):
+			return ErrUnauthorized
+		default:
+			return err
+		}
+	}
+
+	return nil
+}
+
+type CreateStoreFaqInput struct {
+	User     *models.User
+	Store    *models.Store
+	Category *models.FAQCategory
+	Question string
+	Answer   string
+	Language string
+}
+
+type CreateGlobalFaqInput struct {
+	User     *models.User
+	Category *models.FAQCategory
+	Question string
+	Answer   string
+	Language string
+}
+
+type MerchantUpdateFaqInput struct {
+	Merchant *models.User
+	Store    *models.Store
+	FAQID    int
+	Category *models.FAQCategory
+	Question string
+	Answer   string
+	Language string
+}
+
+type AdminUpdateFaqInput struct {
+	Admin    *models.User
+	FAQID    int
+	Category *models.FAQCategory
+	IsGlobal bool
+	Question string
+	Answer   string
+	Language string
 }
